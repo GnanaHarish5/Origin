5403abf005b9f114b5b742d7b46955e81c961917
Gnana Harish <2311cs010367@mallareddyuniversity.ac.in>
2025-01-07 19:36:00 +0530

    Add files via upload

---
+++ Day5_Loops.java
@@ -0,0 +1,20 @@
+public class Day5_Loops{
+    public static void main(String[] args){
+        String c="Harrrrr";
+        System.out.println(c);
+        System.out.println(c.length());
+        System.out.println(c.substring(0, 5));
+        System.out.println(c.charAt(4));
+        System.out.println(c.length()-1);
+        System.out.println("hello".equals("hello"));
+        // To Get Last 3 characters
+        System.out.println(c.substring(c.length()-3));
+        String result=1+"b";
+        System.out.println(result);
+        System.out.println("hello"+2+2);
+        System.out.println("hello"+(2+2));
+        
+
+
+    }
+}
